b6207657e950baae48987d67cb27647206b0f2a0
CVCurcurato <christiancurcurato@gmail.com>
2025-04-11 11:01:53 -0400

    Create Week 1: Preppin' Data 2023 W1.sql

---
+++ Week 1: Preppin' Data 2023 W1.sql
@@ -0,0 +1,37 @@
+-- Preppin' Data 2023 Week 01
+
+-- Split the Transaction Code to extract the letters at the start of the transaction code. These identify the bank who processes the transaction 
+    -- Rename the new field with the Bank code 'Bank'. 
+-- Rename the values in the Online or In-person field, Online of the 1 values and In-Person for the 2 values. 
+-- Change the date to be the day of the week
+-- Different levels of detail are required in the outputs. You will need to sum up the values of the transactions in three ways:
+    -- 1. Total Values of Transactions by each bank
+    -- 2. Total Values by Bank, Day of the Week and Type of Transaction (Online or In-Person)
+    -- 3. Total Values by Bank and Customer Code
+
+-- 1. Total Values of Transactions by each bank
+SELECT REGEXP_SUBSTR(transaction_code, '^[A-Z]{2,3}') AS bank,
+SUM(value) AS value
+FROM PD2023_WK01
+GROUP BY bank
+
+-- 2. Total Values by Bank, Day of the Week and Type of Transaction (Online or In-Person)
+SELECT REGEXP_SUBSTR(transaction_code, '^[A-Z]{2,3}') AS bank,
+CASE WHEN online_or_in_person = 1 THEN 'Online' ELSE 'In-Person' END AS online_or_in_person,
+    CASE WHEN DAYNAME(TO_DATE(LEFT(transaction_date,10), 'DD/MM/YYYY')) = 'Mon' THEN 'Monday'
+         WHEN DAYNAME(TO_DATE(LEFT(transaction_date,10), 'DD/MM/YYYY')) = 'Tue' THEN 'Tuesday'
+         WHEN DAYNAME(TO_DATE(LEFT(transaction_date,10), 'DD/MM/YYYY')) = 'Wed' THEN 'Wednesday'
+         WHEN DAYNAME(TO_DATE(LEFT(transaction_date,10), 'DD/MM/YYYY')) = 'Thu' THEN 'Thursday'
+         WHEN DAYNAME(TO_DATE(LEFT(transaction_date,10), 'DD/MM/YYYY')) = 'Fri' THEN 'Friday'
+         WHEN DAYNAME(TO_DATE(LEFT(transaction_date,10), 'DD/MM/YYYY')) = 'Sat' THEN 'Saturday'
+         ELSE 'Sunday' END AS transaction_date,
+SUM(value) AS value
+FROM PD2023_WK01
+GROUP BY 1,2,3
+
+-- 3. Total Values by Bank and Customer Code
+SELECT REGEXP_SUBSTR(transaction_code, '^[A-Z]{2,3}') AS bank,
+customer_code,
+SUM(value) AS value
+FROM PD2023_WK01
+GROUP BY 1,2
