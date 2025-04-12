047fdf8bd263ff7441b33f803c8dc2671d906a3c
CVCurcurato <christiancurcurato@gmail.com>
2025-04-12 13:10:49 -0400

    Create Week 3: Foodie Fi.sql

---
+++ Week 3: Foodie Fi.sql
@@ -0,0 +1,17 @@
+--A. CUSTOMER JOURNEY
+
+--Briefly describe the first 8 customers’ onboarding journey.
+SELECT plan_name, price, customer_id, start_date,
+    CASE WHEN customer_id = 1 THEN 'Customer switched to basic sub after trial'
+    WHEN customer_id = 2 THEN 'Customer switched to pro sub after trial'
+    WHEN customer_id = 3 THEN 'Customer switched to basic sub after trial'
+    WHEN customer_id = 4 THEN 'Customer switched to basic sub after trial, then attritioned after ~3 months'
+    WHEN customer_id = 5 THEN 'Customer switched to basic sub after trial'
+    WHEN customer_id = 6 THEN 'Customer switched to basic sub after trial, then attritioned after ~2 months'
+    WHEN customer_id = 7 THEN 'Customer switched to basic sub after trial, then pro sub after ~3 months'
+    WHEN customer_id = 8 THEN 'Customer switched to basic sub after trial, then pro sub after ~2 months'
+    ELSE '' END AS onboarding_description
+FROM plans AS P
+INNER JOIN subscriptions AS S
+ON P.plan_id = S.plan_id
+WHERE customer_id <= 8
