196ed78940b359234803f97c326415fed48f93b4
CVCurcurato <christiancurcurato@gmail.com>
2025-04-12 14:43:36 -0400

    Update Week 3: Foodie Fi.sql

--- Week 3: Foodie Fi.sql
+++ Week 3: Foodie Fi.sql
@@ -24,8 +24,16 @@ FROM subscriptions
 -- Result, 1000 customers
 
 -- 2. What is the monthly distribution of trial plan start_date values for our dataset - use the start of the month as the group by value
-SELECT DATE_TRUNC('month',start_date), COUNT(DISTINCT customer_id)
+-- Since the wording is odd, I have interpreted this to mean what is the count of customers starting a trial each month
+SELECT DATE_TRUNC('month',start_date), COUNT(customer_id) AS count_of_customers
 FROM subscriptions
 WHERE plan_id = 0
 GROUP BY 1
 ORDER BY 1
+
+-- 3. What plan start_date values occur after the year 2020 for our dataset? Show the breakdown by count of events for each plan_name
+SELECT start_date, P.plan_name, COUNT(*) AS count_of_events
+FROM subscriptions AS S
+INNER JOIN plans AS P
+WHERE start_date > DATE('2020-12-31')
+GROUP BY 1,2
